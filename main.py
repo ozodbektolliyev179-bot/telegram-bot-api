628bd3b3518b7de5afe8c3a688bab2df4b25d91f
djumanov <djumanovdev@gmail.com>
2026-02-03 19:58:29 +0500

    class based Bot

--- main.py
+++ main.py
@@ -3,39 +3,40 @@ import requests
 from config import TOKEN
 
 
-BASE_URL = F'https://api.telegram.org/bot{TOKEN}'
-
-
-def get_me() -> dict:
-    get_me_url = f'{BASE_URL}/getMe'
+class Bot:
     
-    response = requests.get(get_me_url)
+    def __init__(self, token: str):
+        self.BASE_URL = F'https://api.telegram.org/bot{token}'
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception('Telegram server xatolik qaytardi!')
+    def get_me(self) -> dict:
+        get_me_url = f'{self.BASE_URL}/getMe'
+        
+        response = requests.get(get_me_url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception('Telegram server xatolik qaytardi!')
+
+    def get_updates(self) -> list[dict]:
+        get_updates_url = f'{self.BASE_URL}/getUpdates'
+        
+        response = requests.get(get_updates_url)
+
+        if response.status_code == 200:
+            return response.json()['result']
+        else:
+            raise Exception('Telegram server xatolik qaytardi!')
+        
+    def send_message(self, chat_id: str, text: str) -> None:
+        send_message_url = f'{self.BASE_URL}/sendMessage'
+        
+        params = {
+            'chat_id': chat_id,
+            'text': text
+        }
+        requests.get(send_message_url, params=params)
 
 
-def get_updates() -> list[dict]:
-    get_updates_url = f'{BASE_URL}/getUpdates'
-    
-    response = requests.get(get_updates_url)
-
-    if response.status_code == 200:
-        return response.json()['result']
-    else:
-        raise Exception('Telegram server xatolik qaytardi!')
-    
-
-def send_message() -> None:
-    send_message_url = f'{BASE_URL}/sendMessage'
-    
-    params = {
-        'chat_id': 1258594598,
-        'text': 'Hello World'
-    }
-    requests.get(send_message_url, params=params)
-
-
-send_message()
+bot = Bot(TOKEN)
+bot.send_message(chat_id=1258594598, text='hi')
